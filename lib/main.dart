18d770522bf9f2b7a0a63383f546645ffd6b9c7f
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2023-08-24 22:38:20 +0530

    refresh start

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,85 @@
+import 'dart:io';
 
-import 'dart:developer';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key?key}) super(key:key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckdModeBanner:false(
-      home:
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const Splash(),
+    );
   }
 }
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
 }
 
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Home()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              height: 130,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            if (Platform.isIOS)
+              const CupertinoActivityIndicator(
+                radius: 20,
+              )
+            else
+              const CircularProgressIndicator(
+                color: Colors.black,
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('Home'),
+      ),
+    );
+  }
+}
--- shuttle_service/android/app/src/main/kotlin/com/example/shuttle_service/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.shuttle_service
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
