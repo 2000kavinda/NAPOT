df273baa1ca34a799e643f48d62533d1a9875703
dilshan79 <97287097+dilshan79@users.noreply.github.com>
2023-09-10 13:26:17 +0530

    create text field and style

--- lib/pages/search_student.dart
+++ lib/pages/search_student.dart
@@ -21,6 +21,58 @@ class _SearchStudentState extends State<SearchStudent> {
           onPressed: () {},
         ),
       ),
-    ):
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        //color: Colors.amber,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Student name or ID",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'inter',
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15),
+                const SizedBox(),
+                //
+                TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey[200], // Grey fill color
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color:
+                              Colors.grey), // Set the border color when focused
+                      borderRadius: BorderRadius.circular(
+                          11), // Circular border with radius 11
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors
+                              .grey), // Set the border color when not focused
+                      borderRadius: BorderRadius.circular(
+                          11), // Circular border with radius 11
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 10,
+                    ), // Adjust the vertical and horizontal padding as needed
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+
+    );
   }
 }
