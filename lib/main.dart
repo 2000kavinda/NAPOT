ea097e17272384a60ec59341455f294c4ea4359c
2000kavinda <98000159+2000kavinda@users.noreply.github.com>
2023-09-19 02:34:25 +0530

    admin shuttle complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:untitled1/pages/admin/pendingApproves.dart';
+import 'package:untitled1/pages/admin/shuttles.dart';
 import 'package:untitled1/pages/calendar.dart';
 import 'package:untitled1/pages/canteenAdmin/addItemCanteen.dart';
 import 'package:untitled1/pages/canteenAdmin/canteenItems.dart';
@@ -27,7 +28,7 @@ main() async {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
 
-    home: SignIn(),
+    home: AddBus(),
 
   ));
 }
